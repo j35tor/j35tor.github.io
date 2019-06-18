8e40b51dd7c33a3ca1030821bab624a7eda44f6a
j35tor <30289247+j35tor@users.noreply.github.com>
2019-06-18 12:04:52 +0800

    Update jsplay.js

--- jsplay.js
+++ jsplay.js
@@ -4,15 +4,15 @@ angular.module('stack', [])
 {		$scope.feed = '';
 
         $scope.clear = function()
-                { $scope.feed=''; 
-					document.getElementById("md5feed").innerHTML = '';
-					document.getElementById("sha1feed").innerHTML = '' ;
-				
-				}
+                { 
+		$scope.feed=''; 
+		document.getElementById("md5feed").innerHTML = '';
+		document.getElementById("sha1feed").innerHTML = '' ;
+	
+		}
 
-		$scope.mysha1  = function()
+	$scope.mysha1  = function()
                 {
-                //  alert ('This is mysha1');
                 var mysha1 = new Hashes.SHA1;
                 //=== $scope.sha1feed = mysha1.hex($scope.feed);
                  document.getElementById("sha1feed").innerHTML
@@ -21,18 +21,32 @@ angular.module('stack', [])
 
        $scope.mymd5  = function()
                 {
-                // alert ('This is md5');
                 var mymd5 = new Hashes.MD5;
                 //=== $scope.md5feed = mymd5.hex($scope.feed);
                  document.getElementById("md5feed").innerHTML
                                 = mymd5.hex($scope.feed);
-
-
-                //
-                //  alert ('OK');
                 } // oe_mymd5()
 
-
+	$scope.loc  = function()
+                {
+		if (navigator.geolocation)
+             			{
+                 		navigator.geolocation.getCurrentPosition(showPosition);
+             			}
+             	else {
+                 	myloc.innerHTML = "Geolocation is not supported by this browser.";
+			}
+		
+		function showPosition(position)
+       			{
+         		myloc.innerHTML = "Latitude: " + position.coords.latitude +
+           		"<br>Longitude: " + position.coords.longitude  +
+           		"<br>PlusCode: "  + OpenLocationCode.encode(position.coords.latitude,position.coords.longitude )
+			}
+                
+		
+		
+                } // oe_loc()
 
 
 
