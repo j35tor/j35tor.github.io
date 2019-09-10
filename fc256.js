b648219e594b8bf65cb4a1a1c6a14bf6f170840c
j35tor <30289247+j35tor@users.noreply.github.com>
2019-09-10 15:06:45 +0800

    Update fc256.js  version 0.2

--- fc256.js
+++ fc256.js
@@ -5,20 +5,22 @@ document.getElementById("filetoRead").addEventListener("change",function()
   if (file) {
 
     var reader = new FileReader();
-
-      reader.onload = function (evt)
+    
+    reader.onprogress = updateProgress;
+    reader.onload = function (evt)
       {
         var arrayBuffer = reader.result;
         var gen_checksum = sha256(arrayBuffer);
+        document.getElementById("read_blks").innerHTML = '100 %' ;
         document.getElementById("checksumOut").innerHTML =  gen_checksum ;
 
 
    if   ( gen_checksum == document.getElementById("checksum").value )
-          { document.getElementById("ok").innerHTML = "&#10004;" ;
+          { document.getElementById("ok").innerHTML = "&#10004; CheckSum Good! " ;
             document.getElementById("checksumOut").style.color="#0000FF";
 
           }
-     else { document.getElementById("ok").innerHTML = "&#10007;"
+     else { document.getElementById("ok").innerHTML = "&#10007; CheckSum Failed!!"
             document.getElementById("checksumOut").style.color="#FF4000";}
 
       };
@@ -30,3 +32,17 @@ document.getElementById("filetoRead").addEventListener("change",function()
       }
 
 },false);
+
+function updateProgress(evt) {
+	
+	
+	
+	// evt is an ProgressEvent.
+	if (evt.lengthComputable) {
+		var percentLoaded = Math.round((evt.loaded / evt.total) * 100);
+		// Increase the progress bar length.
+		if (percentLoaded < 100) { 
+        document.getElementById("read_blks").innerHTML =  percentLoaded + '%' ; }
+	    }
+	}
+
