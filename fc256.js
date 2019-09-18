13710add7fd9896221fed6d32878fb4b390a6253
j35tor <30289247+j35tor@users.noreply.github.com>
2019-09-18 16:51:23 +0800

    Update fc256.js

--- fc256.js
+++ fc256.js
@@ -12,17 +12,19 @@ document.getElementById("filetoRead").addEventListener("change",function()
       {
         var arrayBuffer = reader.result;
         var gen_checksum = sha256(arrayBuffer);
+		var feedChecksum = document.getElementById("checksum").value.trim().toLowerCase();
+		
 		document.getElementById("read_blks").innerHTML = '100 %' ;
-		document.getElementById("feedChecksum").innerHTML = document.getElementById("checksum").value ;	
-                document.getElementById("checksumOut").innerHTML =  gen_checksum ;
+		document.getElementById("feedChecksum").innerHTML =  feedChecksum ;	
+        document.getElementById("checksumOut").innerHTML =  gen_checksum ;
 		document.getElementById("checksumOut").style.border="2px solid black";
 
-   if   ( gen_checksum == document.getElementById("checksum").value.trim() )
+   if   ( gen_checksum === feedChecksum )
           { document.getElementById("ok").innerHTML = "CheckSum Matched! &#10004;" ;
             document.getElementById("ok").style.color="#0000FF"; 
 	    document.getElementById("feedChecksum").style.border="2px solid black"; }
 	      
-		else if ( document.getElementById("checksum").value.trim() != "" )
+		else if ( feedChecksum != "" )
 					{ 	document.getElementById("ok").innerHTML = "CheckSum Mismatch !! &#10007;"
 						document.getElementById("ok").style.color="#FF4000"; 
 					 	document.getElementById("feedChecksum").style.border="2px solid black"; 
@@ -64,3 +66,12 @@ function handleDragOver(evt)
 
 
 
+document.getElementById("checksum").addEventListener("mouseover",function()
+{
+    document.getElementById("checksumOut").innerHTML = "" ;
+	document.getElementById("feedChecksum").innerHTML = "" ;
+	document.getElementById("ok").innerHTML = "" ;
+	
+	document.getElementById("checksumOut").style.border="0px solid black";
+	document.getElementById("feedChecksum").style.border="0px solid black";
+},false);
