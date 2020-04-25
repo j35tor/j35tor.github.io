287d49198433d317f350201e6ca3ba15b4f440b8
j35tor <j35tor@email>
2020-04-26 00:40:43 +0800

    Commited on 20200426_00:40

--- webtools/platecal.js
+++ webtools/platecal.js
@@ -6,9 +6,17 @@ angular.module('platecal', [])
           const Fara_constant = 26.801;   //  26.801 A·h/mol
           $scope.metals = [   { Symbol: "NiSO4" , mass:"58.6934" , density: "8.908", valence: "2" },
                               { Symbol: "Acid Cu" , mass:"63.546" , density: "8.96" , valence: "2"  },
+                              { Symbol: "Tin" , mass:"118.710" , density: "7.265" , valence: "2"  },
                               { Symbol: "Au", mass:"196.966570" , density: "19.30" , valence: "1"  },
                               { Symbol: "Ag", mass:"107.8682" , density: "10.49 " , valence: "1"  },
-                              { Symbol: "Pd", mass:"106.42" , density: "12.023" , valence: "1"  },
+                              { Symbol: "Pd", mass:"106.42" , density: "12.023" , valence: "2"  },
+                              { Symbol: "Pt", mass:"195.084" , density: "21.45 " , valence: "4"  },
+                              { Symbol: "ZnCl2", mass:"65.38" , density: "7.14  " , valence: "2"  },
+
+                              { Symbol: "Bi", mass:"208.98040" , density: "9.78   " , valence: "1"  },
+                              { Symbol: "Co", mass:"58.933194" , density: "8.90   " , valence: "1"  },
+                              { Symbol: "Rh", mass:"102.90549" , density: "12.41    " , valence: "1"  },
+
                           ]
 
           $scope.onValueChanged_Metal = function () {
