9eb43d54d09da65247080e9083b29d0740f31450
j35tor <j35tor@email>
2020-06-26 20:02:18 +0800

    Commited on 20200626_20:02

--- webtools/multiclip.js
+++ webtools/multiclip.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function()
 {  var searchParams = new URLSearchParams(window.location.search);
    someid = searchParams.get("ls");
    document.getElementById("localStoreKey").value = someid;
+   check_version();
    reFreshTable(); });
 
 
@@ -19,14 +20,20 @@ document.getElementById("mylocalStore").addEventListener("click",function()
     { document.getElementById(event.target.id).style.color = "blue";
       document.getElementById(event.target.id).parentElement.firstElementChild.nextSibling.style.color = "blue" ;
       document.getElementById("localStoreKey").value =  event.target.id.split("_")[1] ;
-      document.getElementById("feed_box").value = localStorage.getItem( event.target.id.split("_")[1]   );
+	  
+	  document.getElementById("feed_box").value = 
+					localStorage.getItem( ":j35mc:" + document.getElementById("localStoreKeyPage").value 
+							+ ":"  +  event.target.id.split("_")[1] );   	  
+							
     };
 
     if ( event.target.id.substring(0,6) === "lsVal_" )
     { document.getElementById(event.target.id).style.color = "blue";
       document.getElementById(event.target.id).parentElement.firstElementChild.style.color = "blue";
       document.getElementById("localStoreKey").value =  event.target.id.split("_")[1] ;
-      document.getElementById("feed_box").value = localStorage.getItem( event.target.id.split("_")[1] );
+      document.getElementById("feed_box").value = 
+					localStorage.getItem(  ":j35mc:" + document.getElementById("localStoreKeyPage").value 
+							+ ":"  +   event.target.id.split("_")[1]  );
     };
 
 
@@ -34,7 +41,8 @@ document.getElementById("mylocalStore").addEventListener("click",function()
 
 function delPair()
 {
-  var someid1 = document.getElementById("localStoreKey").value ;
+  var someid1 = ":j35mc:" + document.getElementById("localStoreKeyPage").value 
+							+ ":"  + document.getElementById("localStoreKey").value ;
   localStorage.removeItem( someid1 );
   document.getElementById("localStoreKey").value = "";
   document.getElementById("feed_box").value = "";
@@ -53,12 +61,16 @@ function copyTo()
 function storeValue()
 {
   var someid1 = document.getElementById("localStoreKey").value ;
-
   if ( someid1.includes("_") ) { alert ("Key included '_' is NOT allowed");  return; }
   if ( someid1.includes(":") ) { alert ("Key included ':' is NOT allowed");  return; }
   // TODo some users may use ':' before the updated....
+  
+
   if ( someid1 != '' )
           {
+		  someid1 = ":j35mc:" + document.getElementById("localStoreKeyPage").value 
+							+ ":"  + document.getElementById("localStoreKey").value ;		
+			  
           localStorage.setItem( someid1, document.getElementById("feed_box").value );
           reFreshTable(); }
 
@@ -110,6 +122,7 @@ function html_table_header(localID)
 {
   var mylocalStore = document.getElementById(localID);
   document.getElementById(localID).innerHTML = "";
+                                                                                
   var tableHeader =   mylocalStore.insertRow() ;
   var keyHeaderCell = tableHeader.insertCell(0) ;
   keyHeaderCell.innerHTML = "Key" ;
@@ -125,15 +138,48 @@ function reFreshTable()
   {
     var mylocalStore = document.getElementById("mylocalStore");
     html_table_header("mylocalStore");
+	//  var key_page = ":j35mc:"+ document.getElementById("localStoreKeyPage").value + ":"
 
     Object.keys( localStorage ).forEach( function(key,value)
-          { var rowNum = mylocalStore.insertRow();
+	{  if ( key.split(":")[2] != document.getElementById("localStoreKeyPage").value )
+		{ // alert ("skip" + key.split(":")[2] ) ; 
+	      return };
+	   	
+		    var rowNum = mylocalStore.insertRow();
             var keyCell = rowNum.insertCell(0);
-            keyCell.innerHTML = key ;
-            keyCell.id = "lsKey_"+ key ;
+            keyCell.innerHTML =  key.split(":")[3]  ;
+            keyCell.id = "lsKey_"+  key.split(":")[3]  ;
             var localStorgeString = localStorage.getItem( key );
             var keyCell2 = rowNum.insertCell(1);
             keyCell2.innerHTML = localStorgeString.substring(0,10) + '...' ;
-            keyCell2.id = "lsVal_"+ key ;  })
+            keyCell2.id = "lsVal_"+ key.split(":")[3]  ; 
+	} )
 
   } ;  // eo function reFreshTable;
+     
+  
+function  check_version()
+{
+var myversion = localStorage.getItem(":j35mc:_version");
+
+if ( myversion === null )  
+	{ alert ("Multiclip is about to upgrade to 2.0 "); 
+		  Object.keys( localStorage ).forEach( function(key,value) {
+				if (key.substring(0, 6) != ":j35mc:")  
+				{ // alert ( "upgrade " + key );
+					var swap_value = localStorage.getItem( key );
+					localStorage.setItem( ":j35mc:page1:"+key, swap_value );
+					localStorage.removeItem(key);
+				}
+			  
+		  } )
+	alert ("upgrade Done!!");	  
+	localStorage.setItem( ":j35mc:_version", "2.0" );
+
+	 return };
+
+
+// alert ( "Version Checked" ) ;
+};
+  
+function switchPage() { reFreshTable(); }
