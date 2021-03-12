f3640c632c0110a9a6ed122c84516e93c0be2478
j35tor <j35tor@email>
2021-03-12 13:48:29 +0800

    ts:210312_13:48 add xxh64 tools

---
+++ myfch64_data.js
@@ -0,0 +1,10 @@
+const mydata =
+[
+ { "checkSum": "48ca33ca85da3692" , "link" : "/s/9nBc5ECH5JnpcyW"  , "fileName":  "bin.exe" }
+,{ "checkSum": "6c34b1918dba8319" , "link" : "/s/tZxc9SBwCFdTkep"  , "fileName":  "MinGW.exe" }
+,{ "checkSum": "d841e0ee225f2875" , "link" : "/s/3DCPGgPjMHSkiae"  , "fileName":  "node.7z" }
+,{ "checkSum": "733a1ee616e53b8d" , "link" : "/s/8L3n9Gd8dDKj3Yk"  , "fileName":  "_scoop.7z" }
+,{ "checkSum": "3579832d4d93a56c" , "link" : "/s/7Qktb3mW3rZGLtD"  , "fileName":  "atomSetup.exe" }
+,{ "checkSum": "e35f8ca2090f51d0" , "link" : "/s/AB9wcKzJKwgXW3N"  , "fileName":  "DOSBox.7z" }
+,{ "checkSum": "cc85a5b09dcaaad8" , "link" : "/s/4KinGYXKRigCSAF"  , "fileName":  "sysRecCDRTU.exe" }
+]
