3cfcb684501ffd60ae90291d20f4a0e45373a502
j35tor <j35tor@email>
2021-03-18 11:35:29 +0800

    ts:210318_11:35 upgrade multiclip to 2.1

--- webtools/multiclip.js
+++ webtools/multiclip.js
@@ -5,8 +5,13 @@ document.addEventListener("DOMContentLoaded", function()
    someid = searchParams.get("ls");
    document.getElementById("localStoreKey").value = someid;
    check_version();
-   reFreshTable(); });
-
+   
+   // var page = searchParams.get("p") || searchParams.get("page") ;
+   // document.getElementById("localStoreKeyPage").value = page ; 
+    
+   var keyFilter = searchParams.get("kf") || searchParams.get("keyFilter") ;  
+   var valueFilter = searchParams.get("vf") || searchParams.get("valueFilter") ; 
+   reFreshTable( keyFilter , valueFilter);} );
 
 document.getElementById("mylocalStore").addEventListener("click",function()
 {
@@ -34,6 +39,7 @@ document.getElementById("mylocalStore").addEventListener("click",function()
       document.getElementById("feed_box").value = 
 					localStorage.getItem(  ":j35mc:" + document.getElementById("localStoreKeyPage").value 
 							+ ":"  +   event.target.id.split("_")[1]  );
+	  copyTo()
     };
 
 
@@ -90,29 +96,55 @@ if ( document.getElementById("localStoreKey").value === '_killall' )
    document.getElementById("feed_box").value = "" ;
    };
 
-if ( document.getElementById("localStoreKey").value === '_tojson' )
+if ( document.getElementById("localStoreKey").value === '_tojson'  || 
+		document.getElementById("localStoreKey").value === '_tojsonF'
+	)
   {   var myObjs = [] ;
       addItem = Object.create( {} );
       Object.keys( localStorage ).forEach( function(key) {
-             addItem = { "Key" : key , "Value" : localStorage.getItem(key) } ;
+		    if ( key.split(":")[2].includes("_") )  return;  // skip;
+             addItem = { "Key" : key.split(":")[2] + ":" + key.split(":")[3]  
+					, "Value" : localStorage.getItem(key) } ;
              myObjs.push(addItem);  } )  // eo forEach
        var jsonFeed = JSON.stringify(myObjs) ;
-       jsonFeed = jsonFeed.replace("[","'[").replace("]","]'") ;
+	   
+	   if ( document.getElementById("localStoreKey").value === '_tojson' ) 
+			jsonFeed = jsonFeed.replace("[","'[").replace("]","]'") ;
 
       document.getElementById("feed_box").value = jsonFeed ;
   };  // eo _tojson
 
-  if ( document.getElementById("localStoreKey").value === '_fromjson' )
+  if ( document.getElementById("localStoreKey").value === '_fromjson'  ||
+		document.getElementById("localStoreKey").value === '_fromjsonF'
+	)
     {   var myObjs = [] ;  //  alert ("_fromjson") ;
         var jsonFeed = document.getElementById("feed_box").value ;
-        jsonFeed = jsonFeed.replace("'[","[").replace("]'","]") ;
+		
+		if ( document.getElementById("localStoreKey").value === '_fromjson' ) 
+			jsonFeed = jsonFeed.replace("'[","[").replace("]'","]") ;
 
         myObjs =  JSON.parse( jsonFeed );
         Object.keys( myObjs ).forEach( function(key) {
-            localStorage.setItem( myObjs[key].Key , myObjs[key].Value );
+            localStorage.setItem( ":j35mc:" + myObjs[key].Key , myObjs[key].Value );
         } )
         reFreshTable();
     }  // eo _fromjson
+	
+	if ( document.getElementById("localStoreKey").value === '_lspage' ){
+		var myObjs = [] ;
+		addItem = Object.create( {} );
+		Object.keys( localStorage ).forEach( function(key) {
+			 if ( key.split(":")[2].includes("_") )  return;  
+		
+			myObjs.push( key.split(":")[2] );
+			myObjs.sort();
+			var uniqueArray = Array.from(new Set(myObjs));
+			var jsonFeed = JSON.stringify(uniqueArray) ;  
+			document.getElementById("feed_box").value = "Page(s): " +jsonFeed ;		
+		
+			} )
+	} //eo _lspage
+	
     document.getElementById("localStoreKey").value = "" ;
 }
 
@@ -134,17 +166,35 @@ function html_table_header(localID)
 
 
 
-function reFreshTable()
+function reFreshTable(keyFilter, storeFilter)
   {
-    var mylocalStore = document.getElementById("mylocalStore");
+   	
+	var mylocalStore = document.getElementById("mylocalStore");
     html_table_header("mylocalStore");
-	//  var key_page = ":j35mc:"+ document.getElementById("localStoreKeyPage").value + ":"
+	
+	var checkStringKey = new RegExp(keyFilter,"g");
+	if ( keyFilter == null ) { checkStringKey = RegExp("","g") };
+	
+	var checkStringValue = new RegExp(storeFilter,"g");
+	if ( storeFilter == null ) { checkStringValue = RegExp("","g") };
+ 	
+	// alert ( "kf :" + keyFilter + "-->" + checkStringKey );
 
     Object.keys( localStorage ).forEach( function(key,value)
 	{  if ( key.split(":")[2] != document.getElementById("localStoreKeyPage").value )
 		{ // alert ("skip" + key.split(":")[2] ) ; 
 	      return };
-	   	
+		
+		if ( ! checkStringKey.test(key.split(":")[3]) ) 
+				{ 
+				// alert (" test case on " + checkStringKey + ":"  + key.split(":")[3]); 
+				return } ;
+			
+		if 	( ! checkStringValue.test(localStorage.getItem( key ) ) ) 
+		        { 
+				//  alert ("TEST " + localStorage.getItem(key ) ); 
+				return;}
+		
 		    var rowNum = mylocalStore.insertRow();
             var keyCell = rowNum.insertCell(0);
             keyCell.innerHTML =  key.split(":")[3]  ;
@@ -175,8 +225,17 @@ if ( myversion === null )
 		  } )
 	alert ("upgrade Done!!");	  
 	localStorage.setItem( ":j35mc:_version", "2.0" );
+	return };
 
-	 return };
+if ( myversion === "2.0" )
+	{
+	localStorage.setItem( ":j35mc:_version", "2.1" );
+	alert ("Multiclip is about to upgrade to 2.1 \n" +
+            "You may use keyFilter and valueFilter in search box to do fitlering\n" +
+			"e.g. https://j35tor.github.io/webtools/multiclip.html?keyFilter=a&valueFilter=b"
+	);
+		
+	}
 
 
 // alert ( "Version Checked" ) ;
@@ -185,6 +244,7 @@ if ( myversion === null )
 function switchPage() 
 { 
 var someid1 = document.getElementById("localStoreKeyPage").value ;
+
 if ( someid1.includes(":") )
      {
 		  if ( someid1 === ':list' )
