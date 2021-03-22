d4557db787700048b7218933eec4072d79f84f58
j35tor <j35tor@email>
2021-03-22 16:30:29 +0800

    ts:210322_16:30 add miniconda download

--- myfc_data.js
+++ myfc_data.js
@@ -3,13 +3,13 @@ const mydata =
  { "checkSum": "746572d0ba55fdc3a5479aeeb6318b26d33108a0" , "link" : "/s/9nBc5ECH5JnpcyW"  , "fileName":  "bin.exe" }
 ,{ "checkSum": "3bf8ed23e43ea9ae08eddc73bcb11d4fc16dcc29" , "link" : "/s/tZxc9SBwCFdTkep"  , "fileName":  "MinGW.exe" }
 ,{ "checkSum": "1affe9f23360aadefdbee2bec8180de091a84d33" , "link" : "/s/3DCPGgPjMHSkiae"  , "fileName":  "node.7z" }
-,{ "checkSum": "0990b8c42019712733f8ce9f45ecae75b4355caa" , "link" : ""                    , "fileName":  "miniconda3_64.7z" }
+,{ "checkSum": "277c2085ae1bfd3f67f01dcdf932a98ca37aa510" , "link" : "/s/kprtjtMHJEZa4yz"  , "fileName":  "miniconda3_64.7z" }
 ,{ "checkSum": "39cfb67c63ea21052464b894c4b14e1ded46d0e6" , "link" : "/s/qaLETizk8J8go5j"  , "fileName":  "scoop.7z" }
 ,{ "checkSum": "0b78368dbba03bd002c3a65d541387ab74161d08" , "link" : "/s/8L3n9Gd8dDKj3Yk"  , "fileName":  "_scoop.7z" }
 ,{ "checkSum": "3f497acfde9ca35912f0df663226f2a6a097048c" , "link" : "/s/7Qktb3mW3rZGLtD"  , "fileName":  "atomSetup.exe" }
 ,{ "checkSum": "9c668e0be97c5179bab0f580af50f971fd46da83" , "link" : ""                    , "fileName":  "mongodb.zip" }
 ,{ "checkSum": "ad453d63824b6efe1bad7cbbf36ba205fd7d1d88" , "link" : ""                    , "fileName":  "my7z.exe" }
-,{ "checkSum": "44339d9a4303fa0c1e42e16a1d1f439fa7602b8e" , "link" : "/s/2BWrtzJXedKKNtH"  , "fileName":  "emsdk_wrk.7z" }
+,{ "checkSum": "496a5a2f393fc1965bf5e626e5ff661d8296ead2" , "link" : "/s/2BWrtzJXedKKNtH"  , "fileName":  "emsdk_wrk.7z" }
 ,{ "checkSum": "2a02a6a9a3ced101c8fa22125da7d36a907b1027" , "link" : ""                    , "fileName":  "Rset.7z" }
 ,{ "checkSum": "06f23be4ceba35c8c547a98f1b327da20082b483" , "link" : "/s/AB9wcKzJKwgXW3N"  , "fileName":  "DOSBox.7z" }
 ,{ "checkSum": "84fc47494c4703ba54d46f994b8f49280f1eda4b" , "link" : "/s/4KinGYXKRigCSAF"  , "fileName":  "sysRecCDRTU.exe" }
--- myfch32_data.js
+++ myfch32_data.js
@@ -5,7 +5,9 @@ const mydata =
 ,{ "checkSum": "baae1c7c" , "link" : "/s/3DCPGgPjMHSkiae"  , "fileName":  "node.7z" }
 ,{ "checkSum": "2638b53b" , "link" : "/s/8L3n9Gd8dDKj3Yk"  , "fileName":  "_scoop.7z" }
 ,{ "checkSum": "7f2e97cc" , "link" : "/s/b87xtpET9earCXH"  , "fileName":  "scoop1.7z" }
+,{ "checkSum": "83bb8b38" , "link" : "/s/kprtjtMHJEZa4yz"  , "fileName":  "miniconda3_64.7z" }
 ,{ "checkSum": "fc29e586" , "link" : "/s/7Qktb3mW3rZGLtD"  , "fileName":  "atomSetup.exe" }
 ,{ "checkSum": "390ec4db" , "link" : "/s/AB9wcKzJKwgXW3N"  , "fileName":  "DOSBox.7z" }
+,{ "checkSum": "576cff97" , "link" : "/s/2BWrtzJXedKKNtH"  , "fileName":  "emsdk_wrk.7z" }
 ,{ "checkSum": "a4daf9f7" , "link" : "/s/4KinGYXKRigCSAF"  , "fileName":  "sysRecCDRTU.exe" }
 ]
