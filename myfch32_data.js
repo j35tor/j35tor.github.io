6ae8c5f30a63cc7e960a4cf3814c2a18bdef0eff
j35tor <30289247+j35tor@users.noreply.github.com>
2021-08-26 14:17:50 +0800

    ts:210826_1417 update myfch32.data

--- myfch32_data.js
+++ myfch32_data.js
@@ -1,13 +1,14 @@
 const mydata =
 [
- { "checkSum": "c8a99416" , "link" : "/s/PkjdGmrZBPS9Moq"  , "fileName":  "bin.exe" }
+ { "checkSum": "f358f7ab" , "link" : "/s/M2QBAAp5d6rKQYC"  , "fileName":  "bin.exe" }
 ,{ "checkSum": "8ffca8c0" , "link" : "/s/tZxc9SBwCFdTkep"  , "fileName":  "MinGW.exe" }
 ,{ "checkSum": "baae1c7c" , "link" : "/s/3DCPGgPjMHSkiae"  , "fileName":  "node.7z" }
-,{ "checkSum": "2638b53b" , "link" : "/s/8L3n9Gd8dDKj3Yk"  , "fileName":  "_scoop.7z" }
+,{ "checkSum": "08878963" , "link" : "/s/GGraA6pE97mwgQ4"  , "fileName":  "scoop.7z" }
 ,{ "checkSum": "7f2e97cc" , "link" : "/s/b87xtpET9earCXH"  , "fileName":  "scoop1.7z" }
 ,{ "checkSum": "83bb8b38" , "link" : "/s/kprtjtMHJEZa4yz"  , "fileName":  "miniconda3_64.7z" }
 ,{ "checkSum": "fc29e586" , "link" : "/s/7Qktb3mW3rZGLtD"  , "fileName":  "atomSetup.exe" }
 ,{ "checkSum": "390ec4db" , "link" : "/s/AB9wcKzJKwgXW3N"  , "fileName":  "DOSBox.7z" }
 ,{ "checkSum": "576cff97" , "link" : "/s/2BWrtzJXedKKNtH"  , "fileName":  "emsdk_wrk.7z" }
-,{ "checkSum": "a4daf9f7" , "link" : "/s/4KinGYXKRigCSAF"  , "fileName":  "sysRecCDRTU.exe" }
+,{ "checkSum": "a4daf9f7" , "link" : "/s/RYbXKNB8s6dbxqE"  , "fileName":  "sysRecCDRTU.exe" }
+,{ "checkSum": "86c4ee8f" , "link" : "/s/pCz6SELxjbbfnDQ"  , "fileName":  "gitDesk.7z" }
 ]
